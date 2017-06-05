0fb6730ade3415c18b778bda11d915421aef4a1a
Daniel Hambleton <danie@meshconsultants.ca>
2017-06-04 22:12:52 -0400

    fixed some cmake stuff

---
+++ Source/CodeColorer.h
@@ -0,0 +1,19 @@
+#include "Core/Context.h"
+#include "Core/Object.h"
+#include "Container/Vector.h"
+#include "Container/Str.h"
+#include "Core/Variant.h"
+#include "IO/Deserializer.h"
+#include "IO/File.h"
+#include "IO/FileSystem.h"
+
+using namespace Urho3D;
+
+URHO3D_API class CodeColorer : public Object
+{
+	URHO3D_OBJECT(CodeColorer, Object);
+
+public:
+	CodeColorer(Context* context, String path);
+	~CodeColorer() {};
+};
